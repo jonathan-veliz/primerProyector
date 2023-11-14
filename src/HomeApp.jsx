61dcc62d331173061ac6d072ff27c08f46b8ecd7
jonathan-veliz <joniveliz1993@gmail.com>
2023-11-14 17:13:18 -0300

    initial proyect

---
+++ src/HomeApp.jsx
@@ -0,0 +1,17 @@
+
+
+ const name="Lucas"
+  
+   
+   const HomeApp = () => {
+     return (
+      <div>
+        <h1>Hola mundo!</h1>
+        <p>{name}</p>
+      </div>
+
+      
+     )
+   }
+   
+   export default HomeApp
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import  HomeApp  from './HomeApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    < HomeApp/>
+   
+  </React.StrictMode>,
+)
